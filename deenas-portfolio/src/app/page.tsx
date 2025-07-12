f9104079eda93561a6c8d2473984a2b9a2833332
Deena Sun <deenasun@live.com>
2025-07-11 17:10:59 -0700

    Add Galaxy Three.js scene

--- deenas-portfolio/src/app/page.tsx
+++ deenas-portfolio/src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Galaxy from "../components/Galaxy";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="text-center sm:text-left">
+          <h1 className="text-4xl font-bold mb-4">Deena&apos;s Portfolio</h1>
+          <p className="text-lg text-gray-600 dark:text-gray-300 mb-8">
+            Welcome!
+          </p>
+        </div>
+          <Galaxy />
+
         <Image
           className="dark:invert"
           src="/next.svg"
